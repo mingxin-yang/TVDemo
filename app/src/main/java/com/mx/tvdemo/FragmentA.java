a8c5bf74dd8fd42698fe85552d7f82afff039ad8
yangmingxin2016 <1101451857@qq.com>
2017-04-09 22:18:56 +0800

    马希铮王阳第一页面提交

--- app/src/main/java/com/mx/tvdemo/FragmentA.java
+++ app/src/main/java/com/mx/tvdemo/FragmentA.java
@@ -14,22 +14,12 @@ import reco.frame.tv.view.TvGridView;
 
 public class FragmentA extends Fragment {
 	private TvGridView tgv_imagelist;
-//	private TvGridAdapter adapter;
+	//	private TvGridAdapter adapter;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+							 Bundle savedInstanceState) {
 		View parent = inflater.inflate(R.layout.frag_a, container, false);
-		tgv_imagelist = (TvGridView) parent.findViewById(R.id.tgv_imagelist);
-		
-//		List<AppInfo> appList = new ArrayList<AppInfo>();
-//		for (int i = 0; i < 45; i++) {
-//			AppInfo app = new AppInfo();
-//			app.title = "全家盒框架" + i;
-//			appList.add(app);
-//
-//		}
-//		adapter = new TvGridAdapter(getActivity(), appList);
-//		tgv_imagelist.setAdapter(adapter);
+
 		return parent;
 	}
 
